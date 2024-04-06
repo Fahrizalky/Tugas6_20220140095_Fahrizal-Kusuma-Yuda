8e655dd8b3a7143e266468f660ed578365f85d61
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-04-06 20:50:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini Video Dubbing Jawa Upin Ipin");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini Audio Dubbing Jawa Upin Ipin");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini Gambar Dubbing Jawa Upin Ipin");
+});
